a54b934ea33aada20496edd74a0c89c412c0d638
Youssef Ahres <youssefahres@gmail.com>
2014-11-23 11:25:56 -0800

    Fixed few issues with labeling of emission :

--- label_emission_probability.py
+++ label_emission_probability.py
@@ -3,7 +3,7 @@ import viterbi
 from db_wrapper import query_ways_within_radius
 import emission_probability
 
-RADIUS = 20
+RADIUS = 25
 N = 10
 WINDOW = 50
 
@@ -18,13 +18,24 @@ def read_resulting_path(resulting_path_filename):
             results.append((float(line[0]), float(line[1])))
         return results
 
+# Function to read the observations
+def read_observations(filename):
+    observations = []
+    with open(filename) as f:
+        for i, line in enumerate(f):
+            if i == 0:
+                continue
+            line = line.split(',')
+            observations.append((float(line[3]), float(line[4]), float(line[7]), float(line[6])))
+    return observations
+
 # Function to label emission probability training data
 def label_emission_data(file_to_label, **kwargs):
-
     radius = kwargs['radius'] if 'radius' in kwargs else RADIUS
     filename = kwargs['filename'] if 'filename' in kwargs else None
     window = kwargs['window'] if 'window' in kwargs else WINDOW
     n = kwargs['n'] if 'n' in kwargs else N
+
     tmpfile = 'results_file'
     viterbi.run_viterbi(file_to_label,filename=tmpfile, radius=radius, window=window,n=n)
     observations = read_observations(file_to_label)
@@ -32,13 +43,14 @@ def label_emission_data(file_to_label, **kwargs):
     labeled =[]
 
     for t, obs in enumerate(observations):
-        possible_segments=emission_probability.compute_emission_probabilities(obs,radius,n)
-        for seg in possible_segments:
-            node_ids=utils.get_node_ids(seg[2])
+        possible_segments, prob, point = emission_probability.compute_emission_probabilities(obs,radius,n)
+        for t,seg in enumerate(possible_segments):
+            print(seg)
+            node_ids = utils.get_node_ids(seg['endpoints'])
             if node_ids == results[t]:
-                labeled.append((seg[4],seg[5], 1))
+                labeled.append((seg['distance_score'],seg['tangent_score'], 1))
             else:
-                labeled.append((seg[4],seg[5], -1))
+                labeled.append((seg['distance_score'],seg['tangent_score'], -1))
     if filename is not None:
         with open(filename, 'w') as f:
             f.write('Distance Score, Tangent Score, Result\n')
